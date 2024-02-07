7d875ccacb877d370100dd35db4ca5b181e3e70e
aadi58002 <aadi58002@gmail.com>
2024-02-07 15:05:19 +0530

    feat(complete): Complete webhook implementation of echo

--- main.py
+++ main.py
@@ -1,23 +1,67 @@
 from dotenv import load_dotenv
 
 import uvicorn
-import asyncio
+import logging
+import httpx
 
-from api import app
+from fastapi import FastAPI, Request, status, Depends
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from utils import getEnv
 from telegram import TelegramBot
-from utils import get_env
 
+def registerWebHook(telegram_bot: TelegramBot,publicUrl: str):
+    params = {
+        'chat_id': telegram_bot.chat_id,
+        'url': publicUrl,
+    }
 
-if __name__ == "__main__":
-    load_dotenv()
+    try:
+        response = httpx.post(f"{telegram_bot.api_base_url}/setWebhook", data=params)
+        response.raise_for_status()
+        result = response.json()
 
-    token = get_env("TELEGRAM_TOKEN")
-    chat_id = get_env("TELEGRAM_CHAT_ID")
-    public_url = get_env("PUBLIC_URL")
-    local_port = int(get_env("LOCAL_PORT"))
+        if result['ok']:
+            logging.info(f"Webhook setup successfully! for {publicUrl}")
+        else:
+            logging.error(f"Failed to Webhook Setup. Error: {result['description']}")
 
-    telegramBot = TelegramBot(token,chat_id)
+    except Exception as e:
+        logging.error(f"Error during Webhook setup: {e}")
 
-    asyncio.run(telegramBot.register_webhook_async(f"{public_url}:{local_port}"))
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
-    uvicorn.run(app, port=local_port)
+telegramBot = TelegramBot()
+publicUrl = getEnv("PUBLIC_URL")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    logging.error(f"{await request.json()}: {exc_str}")
+    content = {'status_code': 422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+def getTelegramBot():
+    return TelegramBot()
+
+@app.post("/")
+async def getWebHook(request: Request,telegram_bot: TelegramBot = Depends(getTelegramBot)):
+    data = await request.json()
+    print(f"Received data: {data}")
+    telegram_bot.send_message(data['message']['from']['id'],data['message']['text'])
+    return JSONResponse(content={}, status_code=200)
+
+registerWebHook(telegramBot,publicUrl)
+uvicorn.run(app, port=8000)
--- telegram.py
+++ telegram.py
@@ -1,3 +1,4 @@
+from utils import getEnv
 import httpx
 
 class TelegramBot:
@@ -5,25 +6,14 @@ class TelegramBot:
     chat_id: str
     api_base_url: str
 
-    def __init__(self,token: str,chat_id: str):
-        self.token = token 
-        self.chat_id = chat_id 
-        self.api_base_url = f"https://api.telegram.org/bot{self.chat_id}:{self.token}"
+    def __init__(self):
+        self.token = getEnv("TELEGRAM_TOKEN")
+        self.chat_id = self.token.split(':')[0]
+        self.api_base_url = f"https://api.telegram.org/bot{self.token}"
 
-    async def register_webhook_async(self,public_url: str):
-        data = {"url": public_url}
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(f"{self.api_base_url}/setWebhook", json=data)
-                response.raise_for_status()
-                result = response.json()
-                
-                if result['ok']:
-                    print("Webhook registered successfully!")
-                else:
-                    print(f"Failed to register webhook. Error: {result['description']}")
-            
-        except httpx.RequestError as e:
-            print(f"HTTP error occurred: {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+    def send_message(self,chat_id: str,message: str):
+        send_url = f"{self.api_base_url}/sendMessage"
+        print(chat_id,message)
+        data={ 'chat_id': chat_id, 'text': message }
+        with httpx.Client() as client:
+            client.post(send_url,data=data)
